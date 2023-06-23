12aceb39644040e61d948f62247e0e44c9263187
manishkrjaiswal1982 <manishjaiswal.mca@gmail.com>
2023-06-23 21:09:31 +0530

    updated on 20230623 v2

---
+++ js/script.js
@@ -0,0 +1,7 @@
+import jsScroll from "js-scroll-animations";
+import "js-scroll-animations/styles.css";
+
+$(window).on("load", function(){
+    console.log("script loaded....")
+    $(".spinner-grow").hide();
+});
