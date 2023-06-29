fdb2ac00d0f7e4f03192abb2a1a763c6f527dafe
manishkrjaiswal1982 <manishjaiswal.mca@gmail.com>
2023-06-30 01:50:37 +0530

    added preloader and sticy menu functionality

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
-import jsScroll from "js-scroll-animations";
-import "js-scroll-animations/styles.css";
+$(document).ready(function() {
 
-$(window).on("load", function(){
-    console.log("script loaded....")
-    $(".spinner-grow").hide();
+    $(window).on("load", function(){
+        console.log("script loaded....")
+        $(".spinner-grow").hide();
+    });
+
+    $(window).on("scroll", function(){
+    console.log("scrolled....");
+    if (window.scrollY > 50) {
+        $("#menuBar").addClass("fixed-top"); // add padding top to show content behind navbar    
+        } else {
+        $("#menuBar").removeClass("fixed-top");
+        $(document.body).css("padding-top", "0px"); // remove padding top from body                
+      } 
+    });
 });
